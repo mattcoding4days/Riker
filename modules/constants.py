1448088b70030f4f39bd86595b4055d4a8811770
Matt Williams <mattinzhuhai@outlook.com>
2020-04-17 16:09:29 -0700

    Changed colors.py to constants.py, add Devicons icons class

---
+++ modules/constants.py
@@ -0,0 +1,39 @@
+'''
+Ansi escpape colors and ther aesthetically pleasing
+attributes
+'''
+
+#!/usr/bin/env python3
+
+
+class Colors:
+    '''
+    Struct to represent colors
+    '''
+    BBlack = "\033[1;30m"
+    BRed = "\033[1;31m"
+    BGreen = "\033[1;32m"
+    BYellow = "\033[1;33m"
+    BBlue = "\033[1;34m"
+    BPurple = "\033[1;35m"
+    BCyan = "\033[1;36m"
+    BWhite = "\033[1;37m"
+    Reset = "\033[0m"
+
+class Devicons:
+    '''
+    Developer Icons
+    '''
+    Folder = ' '
+    Open_Folder = ' '
+    Git = ' '
+    Arch = ''
+    Ubuntu = ''
+    CentOS = ''
+    Debain = ''
+    Linux = ''
+    Markdown = ''
+    Json = ''
+    Cpp = ''
+    Header = ''
+    Shell = ''
